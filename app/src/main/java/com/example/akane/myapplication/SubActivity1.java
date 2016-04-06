b318355e95c7c36c9da7a191343c9dbbc139b077
recoholic <cahierreco@gmail.com>
2016-04-06 19:08:38 +0900

    メイン画面からサブ画面へ遷移

---
+++ app/src/main/java/com/example/akane/myapplication/SubActivity1.java
@@ -0,0 +1,15 @@
+package com.example.akane.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by akane on 16/04/06.
+ */
+public class SubActivity1 extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sub);
+    }
+}
