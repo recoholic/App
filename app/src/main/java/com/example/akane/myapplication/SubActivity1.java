e0c347c512ac8d211c1ce7518b3090ce6c32c7f5
recoholic <cahierreco@gmail.com>
2016-04-10 00:10:00 +0900

    ImageViewの追加

--- app/src/main/java/com/example/akane/myapplication/SubActivity1.java
+++ app/src/main/java/com/example/akane/myapplication/SubActivity1.java
@@ -2,6 +2,12 @@ package com.example.akane.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import java.io.InputStream;
+import java.io.IOException;
+import android.graphics.BitmapFactory;
+import android.graphics.Bitmap;
+import android.util.Log;
 
 /**
  * Created by akane on 16/04/06.
@@ -11,5 +17,13 @@ public class SubActivity1 extends Activity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sub);
+        ImageView imageView = (ImageView)findViewById(R.id.image_view);
+        try {
+            InputStream istream = getResources().getAssets().open("drago.jpg");
+            Bitmap bitmap = BitmapFactory.decodeStream(istream);
+            imageView.setImageBitmap(bitmap);
+        } catch (IOException e) {
+            Log.d("Assets","Error");
+        }
     }
 }
